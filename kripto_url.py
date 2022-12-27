24f77af232269135498b8ed92b57cf07966081f1
brtkya <121467487+brtkya@users.noreply.github.com>
2022-12-27 04:12:27 +0300

    Add files via upload

---
+++ kripto_url.py
@@ -0,0 +1,20 @@
+def url_mech(): #iki ayrı senaryo için tanımlanmış fonksiyon
+    import webbrowser #gerekli modül
+    url = ("https://www.binance.com/tr/trade/"+currency+"_BUSD?_from=markets&theme=dark&type=spot") #url birleştirme,boşluklara dikkat
+    webbrowser.open(url, new=1, autoraise= True) #gerekli kod
+    exit = input("For exit 0: ")  
+    if exit == 0: #bu eklenmeyince programı bitirmediği için sekmeyi direkt kapatıyor
+        exit()
+
+decision = int(input("Press 0 to choose manually, 1 to see top 5 currencies:"))
+if decision == 1:
+    popular_currencies = {1: "BTC", 2: "DOGE", 3: "AVAX", 4: "ETH", 5: "BNB"} #dictionary
+    print("Top 5 currencies: ", popular_currencies)
+    currency_index = int(input("Enter the index of the currency: ")) 
+    currency = popular_currencies[currency_index] #keyin sırasını girerek value bulma
+    url_mech()
+elif decision == 0:
+    currency = str(input("Enter the code of currency:"))
+    url_mech()
+
+
